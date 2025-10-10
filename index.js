15f74fc2404af38399c38b0ccdf69364953d5cb5
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-10-10 13:28:24 +0530

    connect to mongo db

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import mongoose from "mongoose";
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// mongo db connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("mongo db connect sussfully");
+
+    //   server listinng
+    app.listen(PORT, () => {
+      console.log(`server is listening on ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mongo db connected sussfully :", error);
+    process.exit(1);
+  });
